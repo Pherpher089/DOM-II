164e5bea2eac73563c36c3bb8a72b835d3cab76f
Pherpher089 <Christutor089@gmail.com>
2019-04-12 03:23:09 -0700

    Added in small interaction when hovering over an h4 element. Also working on a button resize animation

--- js/index.js
+++ js/index.js
@@ -23,3 +23,36 @@ let container = document.querySelector('.container.home');
 container.addEventListener('click', function(event){
     console.log(`You clicked on ${event.target} and that is at (${event.screenX}, ${event.screenY}) on the screen`);
 })
+
+/*Image*/
+
+let images = document.querySelectorAll('h2');
+
+images.forEach(element => 
+    element.addEventListener('mouseover', function(event){
+        event.target.className += ' bigText'
+        console.log(event);
+ }))
+
+ images.forEach(element => 
+    element.addEventListener('mouseleave', function(event){
+        event.target.className -= ' bigText';
+        console.log(event);
+ }))
+
+
+ /*Button Animations*/
+ let btns = document.querySelectorAll('.btn');
+
+ btns.forEach(element => 
+    element.addEventListener('mouseover', function(event){
+     event.target.className += ' animateButtonSize'
+     console.log(event);
+ }))
+
+//  btns.forEach(element => 
+//     element.addEventListener('mouseleave', function(event){
+//      event.target.className -= ' animateButtonSize'
+//      event.target.className += ' animateButtonSizeReverse'
+//      console.log(event);
+//  }))
