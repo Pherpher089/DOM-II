c936340678113c9f33c1dd3ae4d820ca088bc113
Pherpher089 <Christutor089@gmail.com>
2019-04-12 01:42:35 -0700

    Added in 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+/*Text Color change on hover*/
+
+let container_p = document.querySelectorAll('p');
+
+container_p.forEach(element => 
+   element.addEventListener('mouseover', function(event){
+    event.target.style.color = 'white';
+    event.target.style.backgroundColor = 'red';
+}))
+
+container_p.forEach(element => 
+    element.addEventListener('mouseleave', function(event){
+     event.target.style.color = 'black';
+     event.target.style.backgroundColor = 'white';
+ }))
+
+/*Container Click event*/
+
+let container = document.querySelector('.container.home');
+
+container.addEventListener('click', function(event){
+    console.log(`You clicked on ${event.target} and that is at (${event.screenX}, ${event.screenY}) on the screen`);
+})
